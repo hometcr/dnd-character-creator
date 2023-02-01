ae29d95a9fdab2a7a4c8f61d2bdefe4338a0d563
Caitlyn <caitlyn.homet@gmail.com>
2023-02-01 15:38:26 -0500

    completed static version of login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,24 @@
-export const Login = () => {
+import logo from "../assets/google-logo.jpg"
 
-  return <div>Login</div>;
-}
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <p>
+        Please log in to access the site:
+      </p>
+      <div className="google-btn">
+        <div className="google-icon-wrapper">
+          <img
+            className="google-icon" 
+            src={logo}
+            alt="google logo"
+          />
+        </div>
+        <p className="btn-text">
+          <b>Sign in with google</b>
+        </p>
+      </div>
+    </div>
+  );
+};
