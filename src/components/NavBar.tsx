9e2536f41cf502976ddeb02b3ceca700a4c8fa13
Caitlyn <caitlyn.homet@gmail.com>
2023-02-01 12:59:23 -0500

    created css for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <div>
-      <Link to="/"> Home </Link>
-      <Link to="/about"> About </Link>
-      <Link to="/login"> Login </Link>
+    <div className="nav-bar">
+      <header className="title">D&D Character Creator</header>
+      <div className = "nav-bar-links">
+        <Link to="/" className="nav-bar-link"> Home </Link>
+        <Link to="/about" className="nav-bar-link"> About </Link>
+        <Link to="/login" className="nav-bar-link"> Login </Link>
+      </div>
     </div>
   );
 }
