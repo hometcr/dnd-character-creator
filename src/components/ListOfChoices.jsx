dcc591f18df254f8dccee9c6680382a458b863e0
Caitlyn <caitlyn.homet@gmail.com>
2023-02-03 18:48:03 -0500

    got ListOfChoices to read through lists of items and convert them to divs/dropdowns as desired

--- src/components/ListOfChoices.jsx
+++ src/components/ListOfChoices.jsx
@@ -2,7 +2,6 @@ export const ListOfChoices = () => {
   //change to props
   let listName = "Proficiencies";
   let knownItems = ["Shovel", "Iron Pot", "Common Clothes"];
-
   let unknownItems = [
     ["Explorer's Pack", "Dungeoneer's Pack"],
     [
@@ -14,36 +13,26 @@ export const ListOfChoices = () => {
     ],
   ];
 
+  // transform known items into listable divs
   let listedKnownItems = knownItems.map((knownItem) => (
     <div className="item-in-choices-list">{knownItem}</div>
   ));
 
-  
-
-  // let listedUnknownItems = unknownItems.map((unknownItem) => (
-  //   <select className="item-select">
-  //     {unknownItem.map((choice) => (
-  //       <option>{choice}</option>
-  //     ))}
-  //   </select>
-  // ));
+  // transform unknown items into listable dropdowns
+  let listedUnknownItems = unknownItems.map((unknownItem) => (
+    <select className="item-select">
+      {unknownItem.map((option) => (
+        <option>{option}</option>
+      ))}
+    </select>
+  ));
 
   return (
     <div className="list-of-choices-and-title">
       <div className="list-of-choices-name">{listName}</div>
       <div className="list-of-choices">
         {listedKnownItems}
-        <select className="item-select">
-          <option>Explorer's Pack</option>
-          <option>Dungeoneer's Pack</option>
-        </select>
-        <select className="item-select">
-          <option>Calligrapher's Supplies</option>
-          <option>Carpenter's Tools</option>
-          <option>Cook's Utensils</option>
-          <option>Mason's Tools</option>
-          <option>Tinker's Tools</option>
-        </select>
+        {listedUnknownItems}
       </div>
     </div>
   );
