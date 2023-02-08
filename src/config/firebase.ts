172bdd8801fb66cde8de459e37b72e6247df4ff0
Caitlyn <caitlyn.homet@gmail.com>
2023-02-07 21:09:48 -0500

    minor change to homepage to test redeployment on firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA20eQPg4EBoDg-SAp7eped5wuN4JUQvL8",
+
+  authDomain: "caitlyns-dnd-character-creator.firebaseapp.com",
+
+  projectId: "caitlyns-dnd-character-creator",
+
+  storageBucket: "caitlyns-dnd-character-creator.appspot.com",
+
+  messagingSenderId: "355453602797",
+
+  appId: "1:355453602797:web:68047ae6e01687a01ab5e4",
+
+  measurementId: "G-Q0HBYJPWZ5",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
