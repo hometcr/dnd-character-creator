7ce31087ce9c821b9f3e541c8ec1bca520dacf15
Caitlyn <caitlyn.homet@gmail.com>
2023-02-10 00:25:14 -0500

    added functionality to character-sheet toggle

--- src/pages/character-sheet/components/DescriptionToggle.tsx
+++ src/pages/character-sheet/components/DescriptionToggle.tsx
@@ -1,27 +1,37 @@
+import { useState } from "react";
+
 interface IProps {
   title: string;
   description: string;
 }
 
 export const DescriptionToggle = (props: IProps) => {
-  let description = props.description;
+  const [toggled, setToggled] = useState(false)
 
   let toggleSelected = false;
 
   let descriptionText = "";
   let triangle = <div></div>;
-  if (toggleSelected) {
-    descriptionText = description;
+  if (toggled) {
+    descriptionText = props.description;
     triangle = <div className="description-toggle-triangle">&#9662;</div>;
   } else {
     descriptionText = "";
     triangle = <div className="description-toggle-triangle">&#9663;</div>;
   }
 
+  const changeToggle = () => {
+    if (toggled) {
+      setToggled(false)
+    } else {
+      setToggled(true)
+    }
+  }
+
   return (
     <div>
       <div className="description-toggle-title-and-triangle">
-        <div className="description-toggle-triangle">{triangle}</div>
+        <div className="description-toggle-triangle" onClick={changeToggle}>{triangle}</div>
         <div className="description-toggle-title">{props.title}</div>
       </div>
       <div className="description-toggle-description">{descriptionText}</div>
