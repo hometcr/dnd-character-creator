907f6eb210316f8c2ed6773b8248505b75160f7f
Caitlyn <caitlyn.homet@gmail.com>
2023-02-12 22:21:25 -0500

    added all? stats to stats.ts, created options.ts to hold stat options

--- src/assets/stats.ts
+++ src/assets/stats.ts
@@ -1,3 +1,5 @@
+import { options } from "./options";
+
 interface ITypeStats {
   bonuses?: {
     Strength?: Number;
@@ -11,12 +13,10 @@ interface ITypeStats {
   unknownSkillProficiencies?: String[][];
   knownItemProficiencies?: String[];
   unknownItemProficiencies?: String[][];
-  knownEquipment?: String[];
-  unknownEquipment?: String[][];
-  knownWeapons?: String[];
-  unknownWeapons?: String[][];
-  knownArmor?: String[];
-  unknownArmor?: String[][];
+  knownItems?: String[];
+  unknownItems?: String[][];
+  knownArmorAndWeapons?: String[];
+  unknownArmorAndWeapons?: String[][];
   knownLanguages?: String[];
   unknownLanguages?: String[][];
   knownCantrips?: String[];
@@ -26,13 +26,25 @@ interface ITypeStats {
 }
 
 export interface IStats {
+  // CLASSES
   Bard: ITypeStats;
-  // currently stats specifically for a Hill Dwarf
-  Dwarf: ITypeStats;
+  Wizard: ITypeStats;
+  Fighter: ITypeStats; // stats for a Dueling Fighter
+  Paladin: ITypeStats;
+  // RACES
+  Dwarf: ITypeStats; // stats for a Hill Dwarf
+  Elf: ITypeStats; // stats for a High Elf
+  Human: ITypeStats;
+  Halfling: ITypeStats; // stats for a Lightfoot Halfling
+  // BACKGROUNDS
   Entertainer: ITypeStats;
+  Acolyte: ITypeStats;
+  "Folk Hero": ITypeStats;
+  Noble: ITypeStats;
 }
 
 export const stats: IStats = {
+  // CLASSES
   Bard: {
     knownItemProficiencies: [
       "Light Armor",
@@ -43,330 +55,145 @@ export const stats: IStats = {
       "Shortswords",
     ],
     unknownItemProficiencies: [
-      [
-        "Bagpipes",
-        "Drum",
-        "Dulcimer",
-        "Flute",
-        "Lute",
-        "Lyre",
-        "Horn",
-        "Pan flute",
-        "Shawn",
-        "Viol",
-      ],
-      [
-        "Bagpipes",
-        "Drum",
-        "Dulcimer",
-        "Flute",
-        "Lute",
-        "Lyre",
-        "Horn",
-        "Pan flute",
-        "Shawn",
-        "Viol",
-      ],
-      [
-        "Bagpipes",
-        "Drum",
-        "Dulcimer",
-        "Flute",
-        "Lute",
-        "Lyre",
-        "Horn",
-        "Pan flute",
-        "Shawn",
-        "Viol",
-      ],
+      options["instruments"],
+      options["instruments"],
+      options["instruments"],
     ],
     knownSkillProficiencies: ["Dexterity", "Charisma"],
+    unknownSkillProficiencies: [
+      options["skills"],
+      options["skills"],
+      options["skills"],
+    ],
+    unknownItems: [
+      ["Diplomat's Pack", "Entertainer's Pack"],
+      options["instruments"],
+    ],
+    knownArmorAndWeapons: ["Dagger", "Leather Armor"],
+    unknownArmorAndWeapons: [
+      ["Rapier", "Longsword", ...options["weapons"]["simpleMelee"]],
+    ],
+    unknownCantrips: [
+      options["spells"]["Bard"]["cantrips"],
+      options["spells"]["Bard"]["cantrips"],
+    ],
+    knownSpells: [],
+    unknownSpells: [
+      options["spells"]["Bard"]["firstLevel"],
+      options["spells"]["Bard"]["firstLevel"],
+      options["spells"]["Bard"]["firstLevel"],
+      options["spells"]["Bard"]["firstLevel"],
+    ],
+  },
+  Wizard: {
+    unknownSkillProficiencies: [
+      ["Arcana", "History", "Insight", "Investigation", "Medicine", "Religion"],
+      ["Arcana", "History", "Insight", "Investigation", "Medicine", "Religion"],
+    ],
+    knownItemProficiencies: [
+      "Daggers",
+      "Darts",
+      "Slings",
+      "Quarterstaffs",
+      "Light Crossbows",
+    ],
+    knownItems: ["Spellbook"],
+    unknownItems: [
+      ["Component Pounch", "Arcane Focus"],
+      ["Scholar's Pack", "Explorer's Pack"],
+    ],
+    unknownArmorAndWeapons: [["Quarterstaff", "Dagger"]],
+    unknownCantrips: [
+      options["spells"]["Wizard"]["cantrips"],
+      options["spells"]["Wizard"]["cantrips"],
+      options["spells"]["Wizard"]["cantrips"],
+    ],
+    unknownSpells: [
+      options["spells"]["Wizard"]["firstLevel"],
+      options["spells"]["Wizard"]["firstLevel"],
+      options["spells"]["Wizard"]["firstLevel"],
+
+      options["spells"]["Wizard"]["firstLevel"],
+      options["spells"]["Wizard"]["firstLevel"],
+      options["spells"]["Wizard"]["firstLevel"],
+    ],
+  },
+  Fighter: {
     unknownSkillProficiencies: [
       [
         "Acrobatics",
         "Animal Handling",
-        "Arcana",
         "Athletics",
-        "Deception",
         "History",
         "Insight",
         "Intimidation",
-        "Investigation",
-        "Medicine",
-        "Nature",
         "Perception",
-        "Performance",
-        "Persuasion",
-        "Religion",
-        "Sleight of Hand",
-        "Stealth",
         "Survival",
       ],
       [
         "Acrobatics",
         "Animal Handling",
-        "Arcana",
         "Athletics",
-        "Deception",
         "History",
         "Insight",
         "Intimidation",
-        "Investigation",
-        "Medicine",
-        "Nature",
         "Perception",
-        "Performance",
-        "Persuasion",
-        "Religion",
-        "Sleight of Hand",
-        "Stealth",
         "Survival",
       ],
+    ],
+    knownItemProficiencies: [
+      "All Armor",
+      "Shields",
+      "Simple Weapons",
+      "Martial Weapons",
+    ],
+    unknownItems: [["Dungeoneer's Pack", "Explorer's Pack"]],
+    unknownArmorAndWeapons: [
+      ["Chain Mail", "Leather Armor, Longbow, and 20 Arrows"],
+      options["weapons"]["martial"],
+      ["Shield", ...options["weapons"]["martial"]],
+      ["Light Crossbow", "Handaxe"],
+    ],
+  },
+  Paladin: {
+    unknownSkillProficiencies: [
       [
-        "Acrobatics",
-        "Animal Handling",
-        "Arcana",
         "Athletics",
-        "Deception",
-        "History",
         "Insight",
         "Intimidation",
-        "Investigation",
         "Medicine",
-        "Nature",
-        "Perception",
-        "Performance",
         "Persuasion",
         "Religion",
-        "Sleight of Hand",
-        "Stealth",
-        "Survival",
+      ],
+      [
+        "Athletics",
+        "Insight",
+        "Intimidation",
+        "Medicine",
+        "Persuasion",
+        "Religion",
       ],
     ],
-    unknownEquipment: [
-      ["Diplomat's Pack", "Entertainer's Pack"],
-      [
-        "Bagpipes",
-        "Drum",
-        "Dulcimer",
-        "Flute",
-        "Lute",
-        "Lyre",
-        "Horn",
-        "Pan Flute",
-        "Shawn",
-        "Viol",
-      ],
+    knownItemProficiencies: [
+      "All armor",
+      "Shields",
+      "Simple Weapons",
+      "Martial Weapons",
     ],
-    knownArmor: ["Leather Armor"],
-    knownWeapons: ["Dagger"],
-    unknownWeapons: [
-      [
-        "Rapier",
-        "Longsword",
-        "Club",
-        "Greatclub",
-        "Handaxe",
-        "Javelin",
-        "Light Hammer",
-        "Mace",
-        "Quarterstaff",
-        "Sickle",
-        "Spear",
-        "Light Crossbow",
-        "Dart",
-        "Shortbow",
-        "Sling",
-      ],
-    ],
-    unknownCantrips: [
-      [
-        "Blade Ward",
-        "Dancing Lights",
-        "Friends",
-        "Light",
-        "Mage Hand",
-        "Mending",
-        "Message",
-        "Minor Illusion",
-        "Prestidigitation",
-        "Thunderclap",
-        "True Strike",
-        "Vicious Mockery",
-      ],
-      [
-        "Blade Ward",
-        "Dancing Lights",
-        "Friends",
-        "Light",
-        "Mage Hand",
-        "Mending",
-        "Message",
-        "Minor Illusion",
-        "Prestidigitation",
-        "Thunderclap",
-        "True Strike",
-        "Vicious Mockery",
-      ],
-    ],
-    knownSpells: [],
-    unknownSpells: [
-      [
-        "Animal Friendship",
-        "Bane",
-        "Charm Person",
-        "Color Spray",
-        "Command",
-        "Comprehend Languages",
-        "Cure Wounds",
-        "Detect Magic",
-        "Disguise Self",
-        "Dissonant Whispers",
-        "Distort Value",
-        "Eart Tremor",
-        "Faerie Fire",
-        "Feather Fall",
-        "Guiding Hand",
-        "Healing Word",
-        "Heroism",
-        "Identity",
-        "Illusory Script",
-        "Longstrider",
-        "Silent Image",
-        "Silvery Barbs",
-        "Sleep",
-        "Speak With Animals",
-        "Tasha's Hideous Laughter",
-        "Thunderwave",
-        "Unseen Servant",
-      ],
-      [
-        "Animal Friendship",
-        "Bane",
-        "Charm Person",
-        "Color Spray",
-        "Command",
-        "Comprehend Languages",
-        "Cure Wounds",
-        "Detect Magic",
-        "Disguise Self",
-        "Dissonant Whispers",
-        "Distort Value",
-        "Eart Tremor",
-        "Faerie Fire",
-        "Feather Fall",
-        "Guiding Hand",
-        "Healing Word",
-        "Heroism",
-        "Identity",
-        "Illusory Script",
-        "Longstrider",
-        "Silent Image",
-        "Silvery Barbs",
-        "Sleep",
-        "Speak With Animals",
-        "Tasha's Hideous Laughter",
-        "Thunderwave",
-        "Unseen Servant",
-      ],
-      [
-        "Animal Friendship",
-        "Bane",
-        "Charm Person",
-        "Color Spray",
-        "Command",
-        "Comprehend Languages",
-        "Cure Wounds",
-        "Detect Magic",
-        "Disguise Self",
-        "Dissonant Whispers",
-        "Distort Value",
-        "Eart Tremor",
-        "Faerie Fire",
-        "Feather Fall",
-        "Guiding Hand",
-        "Healing Word",
-        "Heroism",
-        "Identity",
-        "Illusory Script",
-        "Longstrider",
-        "Silent Image",
-        "Silvery Barbs",
-        "Sleep",
-        "Speak With Animals",
-        "Tasha's Hideous Laughter",
-        "Thunderwave",
-        "Unseen Servant",
-      ],
-      [
-        "Animal Friendship",
-        "Bane",
-        "Charm Person",
-        "Color Spray",
-        "Command",
-        "Comprehend Languages",
-        "Cure Wounds",
-        "Detect Magic",
-        "Disguise Self",
-        "Dissonant Whispers",
-        "Distort Value",
-        "Eart Tremor",
-        "Faerie Fire",
-        "Feather Fall",
-        "Guiding Hand",
-        "Healing Word",
-        "Heroism",
-        "Identity",
-        "Illusory Script",
-        "Longstrider",
-        "Silent Image",
-        "Silvery Barbs",
-        "Sleep",
-        "Speak With Animals",
-        "Tasha's Hideous Laughter",
-        "Thunderwave",
-        "Unseen Servant",
-      ],
+    knownItems: ["Holy Symbol"],
+    unknownItems: [["Priest's Pack", "Explorer's Pack"]],
+    knownArmorAndWeapons: ["Chain Mail"],
+    unknownArmorAndWeapons: [
+      ["Shield", ...options["weapons"]["martial"]],
+      options["weapons"]["martial"],
+      options["weapons"]["simpleMelee"],
     ],
   },
-  Entertainer: {
-    knownItemProficiencies: ["Disguise Kit"],
-    unknownItemProficiencies: [
-      [
-        "Bagpipes",
-        "Drum",
-        "Dulcimer",
-        "Flute",
-        "Lute",
-        "Lyre",
-        "Horn",
-        "Pan flute",
-        "Shawn",
-        "Viol",
-      ],
-    ],
-    knownSkillProficiencies: ["Acrobatics", "Performance"],
-    unknownSkillProficiencies: [[]],
-    knownEquipment: ["The Favor of an Admirer", "Costume"],
-    unknownEquipment: [
-      [
-        "Bagpipes",
-        "Drum",
-        "Dulcimer",
-        "Flute",
-        "Lute",
-        "Lyre",
-        "Horn",
-        "Pan flute",
-        "Shawn",
-        "Viol",
-      ],
-    ],
-  },
+  // RACES
   Dwarf: {
     bonuses: {
       Constitution: 2,
-      Wisdom: 1
+      Wisdom: 1,
     },
     knownItemProficiencies: [
       "Battleaxe",
@@ -378,5 +205,68 @@ export const stats: IStats = {
       ["Smith's Tools", "Brewer's Supplies", "Mason's Tools"],
     ],
     knownLanguages: ["Common", "Dwarvish"],
+  },
+  Elf: {
+    bonuses: {
+      Dexterity: 2,
+      Intelligence: 1,
+    },
+    knownSkillProficiencies: ["Perception"],
+    knownItemProficiencies: ["Longsword", "Shortsword", "Shortbow", "Longbow"],
+    knownLanguages: ["Common", "Elven"],
+    unknownLanguages: [options["languages"]],
+    unknownCantrips: [options["spells"]["Wizard"]["cantrips"]],
+  },
+  Human: {
+    bonuses: {
+      Strength: 1,
+      Dexterity: 1,
+      Intelligence: 1,
+      Charisma: 1,
+      Constitution: 1,
+      Wisdom: 1,
+    },
+    knownLanguages: ["Common"],
+    unknownLanguages: [options["languages"]],
+  },
+  Halfling: {
+    bonuses: {
+      Dexterity: 2,
+      Charisma: 1,
+    },
+    knownLanguages: ["Common", "Halfling"],
+  },
+  // BACKGROUNDS
+  Entertainer: {
+    knownItemProficiencies: ["Disguise Kit"],
+    unknownItemProficiencies: [options["instruments"]],
+    knownSkillProficiencies: ["Acrobatics", "Performance"],
+    unknownSkillProficiencies: [[]],
+    knownItems: ["The Favor of an Admirer", "Costume"],
+    unknownItems: [options["instruments"]],
+  },
+  Acolyte: {
+    knownSkillProficiencies: ["Insight", "Religion"],
+    unknownLanguages: [options["languages"], options["languages"]],
+    knownItems: [
+      "Holy Symbol",
+      "Prayer Book",
+      "Incence",
+      "Vestements",
+      "Common Clothes",
+    ],
+  },
+  "Folk Hero": {
+    knownSkillProficiencies: ["Animal Handling", "Survival"],
+    knownItemProficiencies: ["Land Vehicles"],
+    unknownItemProficiencies: [options["artisansTools"]],
+    knownItems: ["Shovel", "Iron Pot", "Common Clothes"],
+    unknownItems: [options["artisansTools"]],
+  },
+  Noble: {
+    knownSkillProficiencies: ["History", "Persuasion"],
+    unknownItems: [["Dice Set", "Playing Card Set"]],
+    unknownLanguages: [options["languages"]],
+    knownItems: ["Fine Clothes", "Signet Ring", "Scroll of Pedigree"],
   },
 };
